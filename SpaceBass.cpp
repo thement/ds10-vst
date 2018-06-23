033f2b85d7aa916d11812040b0c70dc4599cddbc
Jakub Horak <thement@ibawizard.net>
2018-06-23 08:09:12 +0200

    Remove dead code

--- SpaceBass.cpp
+++ SpaceBass.cpp
@@ -148,6 +148,7 @@ void SpaceBass::CreateParams() {
 		case PVoices4:
 		case PModsrc7:
 			par->InitEnum(name, param->def, param->max - param->min + 1);
+			par->SetDisplayText(0, param->name);
 			break;
 		}
 	}
@@ -196,99 +197,6 @@ void SpaceBass::CreateGraphics() {
 	AttachGraphics(pGraphics);
 }
 
-#if 0
-void SpaceBass::CreateParams() {
-  for (int i = 0; i < kNumParams; i++) {
-    IParam* param = GetParam(i);
-    const parameterProperties_struct& properties = parameterProperties[i];
-    switch (i) {
-        // Enum Parameters:
-      case mOsc1Waveform:
-      case mOsc2Waveform:
-        param->InitEnum(properties.name,
-                        Oscillator::OSCILLATOR_MODE_SAW,
-                        Oscillator::kNumOscillatorModes);
-        // For VST3:
-        param->SetDisplayText(0, properties.name);
-        break;
-      case mLFOWaveform:
-        param->InitEnum(properties.name,
-                        Oscillator::OSCILLATOR_MODE_TRIANGLE,
-                        Oscillator::kNumOscillatorModes);
-        // For VST3:
-        param->SetDisplayText(0, properties.name);
-        break;
-      case mFilterMode:
-        param->InitEnum(properties.name,
-                        Filter::FILTER_MODE_LOWPASS,
-                        Filter::kNumFilterModes);
-        break;
-        // Double Parameters:
-      default:
-        param->InitDouble(properties.name,
-                          properties.defaultVal,
-                          properties.minVal,
-                          properties.maxVal,
-                          parameterStep);
-        break;
-    }
-  }
-  GetParam(mFilterCutoff)->SetShape(2);
-  GetParam(mVolumeEnvAttack)->SetShape(3);
-  GetParam(mFilterEnvAttack)->SetShape(3);
-  GetParam(mVolumeEnvDecay)->SetShape(3);
-  GetParam(mFilterEnvDecay)->SetShape(3);
-  GetParam(mVolumeEnvSustain)->SetShape(2);
-  GetParam(mFilterEnvSustain)->SetShape(2);
-  GetParam(mVolumeEnvRelease)->SetShape(3);
-  GetParam(mFilterEnvRelease)->SetShape(3);
-  for (int i = 0; i < kNumParams; i++) {
-    OnParamChange(i);
-  }
-}
-
-void SpaceBass::CreateGraphics() {
-  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
-  pGraphics->AttachBackground(BG_ID, BG_FN);
-#ifdef KEYBOARD_ENABLED
-  IBitmap whiteKeyImage = pGraphics->LoadIBitmap(WHITE_KEY_ID, WHITE_KEY_FN, 6);
-  IBitmap blackKeyImage = pGraphics->LoadIBitmap(BLACK_KEY_ID, BLACK_KEY_FN);
-  //                            C#     D#          F#      G#      A#
-  int keyCoordinates[12] = { 0, 10, 17, 30, 35, 52, 61, 68, 79, 85, 97, 102 };
-  mVirtualKeyboard = new IKeyboardControl(this, kKeybX, kKeybY, virtualKeyboardMinimumNoteNumber, /* octaves: */ 4, &whiteKeyImage, &blackKeyImage, keyCoordinates);
-  pGraphics->AttachControl(mVirtualKeyboard);
-#endif
-  IBitmap waveformBitmap = pGraphics->LoadIBitmap(WAVEFORM_ID, WAVEFORM_FN, 4);
-  IBitmap filterModeBitmap = pGraphics->LoadIBitmap(FILTERMODE_ID, FILTERMODE_FN, 3);
-  IBitmap knobBitmap = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, 128);
-  for (int i = 0; i < kNumParams; i++) {
-    const parameterProperties_struct& properties = parameterProperties[i];
-    IControl* control;
-    IBitmap* graphic;
-    switch (i) {
-        // Switches:
-      case mOsc1Waveform:
-      case mOsc2Waveform:
-      case mLFOWaveform:
-        graphic = &waveformBitmap;
-        control = new ISwitchControl(this, properties.x, properties.y, i, graphic);
-        break;
-      case mFilterMode:
-        graphic = &filterModeBitmap;
-        control = new ISwitchControl(this, properties.x, properties.y, i, graphic);
-        break;
-        // Knobs:
-      default:
-        graphic = &knobBitmap;
-        control = new IKnobMultiControl(this, properties.x, properties.y, i, graphic);
-        break;
-    }
-    pGraphics->AttachControl(control);
-  }
-  AttachGraphics(pGraphics);
-}
-#endif
-
 void SpaceBass::CreatePresets() {
 }
 
@@ -301,9 +209,7 @@ void SpaceBass::ProcessDoubleReplacing(
   
   double *leftOutput = outputs[0];
   double *rightOutput = outputs[1];
-#ifdef KEYBOARD_ENABLED
-  processVirtualKeyboard();
-#endif
+
   for (int i = 0; i < nFrames; ++i) {
     mMIDIReceiver.advance();
     leftOutput[i] = rightOutput[i] = ds10_get_sample();
@@ -405,30 +311,4 @@ void SpaceBass::OnParamChange(int paramIdx)
 
 void SpaceBass::ProcessMidiMsg(IMidiMsg* pMsg) {
   mMIDIReceiver.onMessageReceived(pMsg);
-#ifdef KEYBOARD_ENABLED
-  mVirtualKeyboard->SetDirty();
-#endif
 }
-
-#ifdef KEYBOARD_ENABLED
-void SpaceBass::processVirtualKeyboard() {
-  IKeyboardControl* virtualKeyboard = (IKeyboardControl*) mVirtualKeyboard;
-  int virtualKeyboardNoteNumber = virtualKeyboard->GetKey() + virtualKeyboardMinimumNoteNumber;
-  
-  if(lastVirtualKeyboardNoteNumber >= virtualKeyboardMinimumNoteNumber && virtualKeyboardNoteNumber != lastVirtualKeyboardNoteNumber) {
-    // The note number has changed from a valid key to something else (valid key or nothing). Release the valid key:
-    IMidiMsg midiMessage;
-    midiMessage.MakeNoteOffMsg(lastVirtualKeyboardNoteNumber, 0);
-    mMIDIReceiver.onMessageReceived(&midiMessage);
-  }
-  
-  if (virtualKeyboardNoteNumber >= virtualKeyboardMinimumNoteNumber && virtualKeyboardNoteNumber != lastVirtualKeyboardNoteNumber) {
-    // A valid key is pressed that wasn't pressed the previous call. Send a "note on" message to the MIDI receiver:
-    IMidiMsg midiMessage;
-    midiMessage.MakeNoteOnMsg(virtualKeyboardNoteNumber, virtualKeyboard->GetVelocity(), 0);
-    mMIDIReceiver.onMessageReceived(&midiMessage);
-  }
-  
-  lastVirtualKeyboardNoteNumber = virtualKeyboardNoteNumber;
-}
-#endif
